d007d1da5e4f03eed1b289631d449eab9486ec80
bluelightning40 <39336921+bluelightning40@users.noreply.github.com>
2022-09-10 23:05:12 +0700

    addNewCustomer, addNewUser

--- Server App/index.js
+++ Server App/index.js
@@ -21,6 +21,7 @@ let date_ob = new Date();
 // app.use("/",user);
 
 app.use(bodyParser.urlencoded({extended:false}));
+app.use('/uploads', express.static('uploads'));
 
 app.use(cors());
 
@@ -65,7 +66,155 @@ function checkFileType(file,cb){
   }
 }
 
+//========== Customer ==========
+
+app.get("/api/list-customer", (req,res) => {
+  pool.getConnection((err,conn)=>{
+    conn.query(`select * from customer`, (err,result)=>{
+      if(err) return res.status(500).send(err)
+      else{
+        return res.status(200).send(result);
+      }
+    })
+    conn.release();
+  })
+});
+
+app.post("/api/addNewCustomer", (req,res) =>{
+
+  var date = date_ob.getDate();
+  var month = date_ob.getMonth()+1;
+  var year = date_ob.getYear() % 100;
+
+  var id = "C";
+  var name = req.body.name;
+  var phone_number = req.body.phone_number;
+  var email = req.body.email;
+  var address = req.body.address;
+  var create_id = "CC";
+  var create_ip = req.socket.localAddress;
+  var notes = req.body.notes;
+  var status = req.body.status;
+
+  var count_id = 0;
+
+  if(date<10){
+    id = id + '0' + date;
+    create_id = create_id + '0' + date;
+  }
+  else{
+    id = id+date;
+    create_id = create_id+date;
+  }
+
+  if(month<10){
+    id = id + '0' + month;
+    create_id = create_id + '0' + month;
+  }
+  else{
+    id = id+month;
+    create_id = create_id+month;
+  }
+
+  if(year<10){
+    id = id + '0' + year;
+    create_id = create_id + '0' + year;
+  }
+  else{
+    id = id+year;
+    create_id = create_id+year;
+  }
+
+  pool.getConnection((err,conn)=>{
+    conn.query(`select * from customer where customer_id LIKE '%${id}'`, function(req,result){
+      if(err) return res.status(500).send(err)
+      else{
+        count_id = result.length +1;
+
+        if(count_id<10){
+          id = id+'000'+count_id;
+          create_id = create_id+'00'+count_id;
+        }
+        else if(count_id<100){
+          id = id+'00'+count_id;
+          create_id = create_id+'0'+count_id;
+        }
+        else{
+          id=id+'0'+count_id;
+          create_id=create_id+count_id;
+        }
+
+        var sql = `INSERT INTO CUSTOMER (customer_id,customer_name,customer_phone_number,customer_email,customer_address,customer_create_id, customer_create_ip, customer_notes,customer_status) VALUES ('${id}','${name}','${phone_number}','${email}','${address}','${create_id}','${create_ip}','${notes}','${status}')`
+
+        pool.getConnection((err,conn)=>{
+          conn.query(sql,function(req,result){
+            if(err) return res.status(500).send(err)
+            else{
+              return res.status(200).send(id);
+            }
+          })
+          conn.release();
+        })
+      }
+    })
+    conn.release();
+  })
+
+});
+
+//========== Product ==========
+
+app.get("/api/list-product", (req,res) => {
+  pool.getConnection((err,conn)=>{
+    conn.query(`select * from product where product_category='produk'`, (err,result)=>{
+      if(err) return res.status(500).send(err)
+      else{
+        return res.status(200).send(result);
+      }
+    })
+    conn.release();
+  })
+});
+
+//========== Service ==========
+
+app.get("/api/list-service", (req,res) => {
+  pool.getConnection((err,conn)=>{
+    conn.query(`select * from product where product_category='jasa'`, (err,result)=>{
+      if(err) return res.status(500).send(err)
+      else{
+        return res.status(200).send(result);
+      }
+    })
+    conn.release();
+  })
+});
+
 //========== Transaction ==========
+
+app.get("/api/list-h-trans", (req,res) => {
+  pool.getConnection((err,conn)=>{
+    conn.query(`select * from h_trans`, (err,result)=>{
+      if(err) return res.status(500).send(err)
+      else{
+        return res.status(200).send(result);
+      }
+    })
+    conn.release();
+  })
+});
+
+app.get("/api/list-d-trans", (req,res) => {
+  pool.getConnection((err,conn)=>{
+    conn.query(`select * from d_trans`, (err,result)=>{
+      if(err) return res.status(500).send(err)
+      else{
+        return res.status(200).send(result);
+      }
+    })
+    conn.release();
+  })
+});
 
 app.post('/api/addNewTransaction', function(req,res){
 
@@ -228,73 +377,81 @@ app.post('/api/addNewTransactionDetail', function(req,res){
   })
 });
 
-//========== Customer ==========
+//untested
+app.post('/api/uploadTransactionImage', upload.single('uploaded-image'), function(req,res){
+  var image_type = req.query.image_type;
+  var image_notes = req.body.image_notes;
+  var image_name = filename;
+  var d_trans_id = req.query.d_trans_id;
 
-app.get("/api/list-customer", (req,res) => {
-  pool.getConnection((err,conn)=>{
-    conn.query(`select * from customer`, (err,result)=>{
-      if(err) return res.status(500).send(err)
-      else{
-        return res.status(200).send(result);
-      }
+  if(image_type == 'main'){
+    var sql = `UPDATE 'd_trans' set d_trans_main_photo = '${image_name}', d_trans_main_note = '${image_notes}' where d_trans_id = '${d_trans_id}'`;
+
+    pool.getConnection((err,conn)=>{
+      conn.query(sql,function(req,result){
+        if(err) return res.status(500).send(err)
+        else{
+          return res.status(200).send(result);
+        }
+      })
+      conn.release();
     })
-    conn.release();
-  })
+  }
+  else if(image_type == 'top'){
+    var sql = `UPDATE 'd_trans' set d_trans_top_photo = '${image_name}', d_trans_top_note = '${image_notes}' where d_trans_id = '${d_trans_id}'`;
+
+    pool.getConnection((err,conn)=>{
+      conn.query(sql,function(req,result){
+        if(err) return res.status(500).send(err)
+        else{
+          return res.status(200).send(result);
+        }
+      })
+      conn.release();
+    })
+  }
+  else if(image_type == 'below'){
+    var sql = `UPDATE 'd_trans' set d_trans_below_photo = '${image_name}', d_trans_below_note = '${image_notes}' where d_trans_id = '${d_trans_id}'`;
+
+    pool.getConnection((err,conn)=>{
+      conn.query(sql,function(req,result){
+        if(err) return res.status(500).send(err)
+        else{
+          return res.status(200).send(result);
+        }
+      })
+      conn.release();
+    })
+  }
+  else if(image_type == 'left'){
+    var sql = `UPDATE 'd_trans' set d_trans_left_photo = '${image_name}', d_trans_left_note = '${image_notes}' where d_trans_id = '${d_trans_id}'`;
+
+    pool.getConnection((err,conn)=>{
+      conn.query(sql,function(req,result){
+        if(err) return res.status(500).send(err)
+        else{
+          return res.status(200).send(result);
+        }
+      })
+      conn.release();
+    })
+  }
+  else if(image_type == 'right'){
+    var sql = `UPDATE 'd_trans' set d_trans_right_photo = '${image_name}', d_trans_right_note = '${image_notes}' where d_trans_id = '${d_trans_id}'`;
+
+    pool.getConnection((err,conn)=>{
+      conn.query(sql,function(req,result){
+        if(err) return res.status(500).send(err)
+        else{
+          return res.status(200).send(result);
+        }
+      })
+      conn.release();
+    })
+  }
+
 });
 
-//========== Product ==========
-
-app.get("/api/list-product", (req,res) => {
-  pool.getConnection((err,conn)=>{
-    conn.query(`select * from product where product_category='produk'`, (err,result)=>{
-      if(err) return res.status(500).send(err)
-      else{
-        return res.status(200).send(result);
-      }
-    })
-    conn.release();
-  })
-});
-
-//========== Service ==========
-
-app.get("/api/list-service", (req,res) => {
-  pool.getConnection((err,conn)=>{
-    conn.query(`select * from product where product_category='jasa'`, (err,result)=>{
-      if(err) return res.status(500).send(err)
-      else{
-        return res.status(200).send(result);
-      }
-    })
-    conn.release();
-  })
-});
-
-//========== Transaction ==========
-
-app.get("/api/list-h-trans", (req,res) => {
-  pool.getConnection((err,conn)=>{
-    conn.query(`select * from h_trans`, (err,result)=>{
-      if(err) return res.status(500).send(err)
-      else{
-        return res.status(200).send(result);
-      }
-    })
-    conn.release();
-  })
-});
-
-app.get("/api/list-d-trans", (req,res) => {
-  pool.getConnection((err,conn)=>{
-    conn.query(`select * from d_trans`, (err,result)=>{
-      if(err) return res.status(500).send(err)
-      else{
-        return res.status(200).send(result);
-      }
-    })
-    conn.release();
-  })
-});
 //========== User ==========
 
 app.get("/api/list-user", (req,res) => {
@@ -307,6 +464,86 @@ app.get("/api/list-user", (req,res) => {
     })
     conn.release();
   })
+});
+
+app.post("/api/addNewUser", (req,res) =>{
+
+  var date = date_ob.getDate();
+  var month = date_ob.getMonth()+1;
+  var year = date_ob.getYear() % 100;
+
+  var id = "U";
+  var name = req.body.name;
+  var password = req.body.password;
+  var create_id = "UC";
+  var create_ip = req.socket.localAddress;
+  var notes = req.body.notes;
+  var status = req.body.status;
+
+  var count_id = 0;
+
+  if(date<10){
+    id = id + '0' + date;
+    create_id = create_id + '0' + date;
+  }
+  else{
+    id = id+date;
+    create_id = create_id+date;
+  }
+
+  if(month<10){
+    id = id + '0' + month;
+    create_id = create_id + '0' + month;
+  }
+  else{
+    id = id+month;
+    create_id = create_id+month;
+  }
+
+  if(year<10){
+    id = id + '0' + year;
+    create_id = create_id + '0' + year;
+  }
+  else{
+    id = id+year;
+    create_id = create_id+year;
+  }
+
+  pool.getConnection((err,conn)=>{
+    conn.query(`select * from user where user_id LIKE '%${id}'`, function(req,result){
+      if(err) return res.status(500).send(err)
+      else{
+        count_id = result.length +1;
+
+        if(count_id<10){
+          id = id+'000'+count_id;
+          create_id = create_id+'00'+count_id;
+        }
+        else if(count_id<100){
+          id = id+'00'+count_id;
+          create_id = create_id+'0'+count_id;
+        }
+        else{
+          id=id+'0'+count_id;
+          create_id=create_id+count_id;
+        }
+
+        var sql = `INSERT INTO USER (user_id,user_name,user_password,user_create_id,user_create_ip,user_notes, user_status) VALUES ('${id}','${name}','${password}','${create_id}','${create_ip}','${notes}','${status}')`
+
+        pool.getConnection((err,conn)=>{
+          conn.query(sql,function(req,result){
+            if(err) return res.status(500).send(err)
+            else{
+              return res.status(200).send(id);
+            }
+          })
+          conn.release();
+        })
+      }
+    })
+    conn.release();
+  })
+
 });
 
 // ===========================================================================================
